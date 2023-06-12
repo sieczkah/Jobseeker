b305d23bc642d5c2174b365bb2359ce76a7901af
sieczkah <huberts94@gmail.com>
2023-06-12 20:28:31 +0200

    Added Users app and authentication system

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("accounts/login/", views.login_view, name="login"),
+    path("accounts/register/", views.register_view, name="register"),
+    path("accounts/logout/", views.logout_view, name="logout"),
+]
---
+++ users/views.py
@@ -0,0 +1,52 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.views.decorators.debug import sensitive_variables
+
+
+# Create your views here.
+@sensitive_variables("username", "pw")
+def login_view(request):
+    # Page name to handle the login/register template
+    page = "login"
+
+    # Avoiding log in users login page via url
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    # Checking if request is POST, authenticating user and login in if authenticated
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            redirect_link = request.GET.get("next", default="home")
+            return redirect(redirect_link)
+    else:
+        form = AuthenticationForm()
+
+    context = {"page": page, "form": form}
+    return render(request, "users/login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form}
+    return render(request, "users/login.html", context)
