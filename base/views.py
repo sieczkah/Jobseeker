afcfb49a8bf56b2ceb1618e9fec36e7a666ec8aa
sieczkah <huberts94@gmail.com>
2023-06-20 16:28:35 +0200

    Deleted unnecessary line in JobOfferDetail view

--- base/views.py
+++ base/views.py
@@ -73,8 +73,6 @@ class JobOfferDetail(LoginRequiredMixin, DetailView):
     model = JobOffer
     context_object_name = "job_offer"
 
-    user_is_pro = "pr"
-
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
 
